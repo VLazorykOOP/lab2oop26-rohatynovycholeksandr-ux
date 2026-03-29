f9a3a8d9dfb4c3ad27f38911b84b3ea8a5fae266
rohatynovycholeksandr-ux <rohatynovych.oleksandr@chnu.edu.ua>
2026-03-29 21:17:12 +0300

    Refactor encryption and decryption functions

--- OOPLab2Tpl/OOPLab2Tpl.cpp
+++ OOPLab2Tpl/OOPLab2Tpl.cpp
@@ -4,6 +4,9 @@
 #include <vector>
 using namespace std;
 
+const int ROWS = 16;
+const int COLS = 4;
+
 int parity(unsigned short x, int l, int r)
 {
     int p = 0;
@@ -12,163 +15,202 @@ int parity(unsigned short x, int l, int r)
 }
 
 // ---------------- TASK 1 ----------------
+// 2049*b + (15*d + 12*a)/2048 - 100*c + 104*d
 void task1()
 {
     int a, b, c, d;
     cout << "Enter a b c d: ";
     cin >> a >> b >> c >> d;
 
-    int result =
-        ((b << 11) + b) +                                  
-        ((((d << 4) - d) + ((a << 3) + (a << 2))) >> 11) - 
-        ((c << 6) + (c << 5) + (c << 2)) +               
-        ((d << 6) + (d << 5) + (d << 3));                  
+    int res = ((b << 11) + b)
+        + ((((d << 4) - d) + ((a << 3) + (a << 2))) >> 11)
+        - ((c << 6) + (c << 5) + (c << 2))
+        + ((d << 6) + (d << 5) + (d << 3));
 
-    cout << "Result = " << result << endl;
+    cout << "Result = " << res << endl;
 }
 
-// ---------------- TASK 2 ENCRYPT ----------------
-unsigned short encodeChar(int row, int pos, unsigned char ch)
+// ---------------- INPUT ----------------
+vector<string> inputText()
+{
+    vector<string> s(ROWS);
+    cin.ignore();
+    cout << "Enter 16 lines (up to 4 chars):\n";
+    for (int i = 0; i < ROWS; i++)
+    {
+        getline(cin, s[i]);
+        if (s[i].size() > 4) s[i] = s[i].substr(0, 4);
+        while (s[i].size() < 4) s[i] += ' ';
+    }
+    return s;
+}
+
+// ---------------- TASK 2 ----------------
+unsigned short encode(int row, int pos, unsigned char ch)
 {
     unsigned short w = 0;
     int hi = (ch >> 4) & 15;
     int lo = ch & 15;
 
-    w |= row;          // bits 0-3                 0 00 0 000 pr hi0000 0011 
-    w |= hi << 4;      // bits 4-7
-    w |= parity(w, 0, 7) << 8;   // bit 8
-    w |= lo << 9;      // bits 9-12
-    w |= pos << 13;    // bits 13-14
-    w |= parity(w, 9, 14) << 15; // bit 15
+    w |= row;                 // 0-3
+    w |= hi << 4;             // 4-7
+    w |= parity(w, 0, 7) << 8;
+    w |= lo << 9;             // 9-12
+    w |= pos << 13;           // 13-14
+    w |= parity(w, 9, 14) << 15;
 
     return w;
 }
 
-void encryptText()
+void encryptBitwise()
 {
-    cin.ignore();
-    ofstream fout("data.bin", ios::binary);
+    vector<string> s = inputText();
+    ofstream f("bitwise.bin", ios::binary);
 
-    cout << "Enter 16 lines (up to 4 chars):\n";
-    for (int i = 0; i < 16; i++)
-    {
-        string s;
-        getline(cin, s);
-        while (s.size() < 4) s += ' ';
-        if (s.size() > 4) s = s.substr(0, 4);
-
-        for (int j = 0; j < 4; j++)
+    for (int i = 0; i < ROWS; i++)
+        for (int j = 0; j < COLS; j++)
         {
-            unsigned short w = encodeChar(i, j, s[j]);
-            fout.write((char*)&w, sizeof(w));
+            unsigned short w = encode(i, j, s[i][j]);
+            f.write((char*)&w, sizeof(w));
         }
-    }
 
-    fout.close();
-    cout << "Encrypted to data.bin\n";
+    f.close();
+    cout << "Saved to bitwise.bin\n";
 }
 
-// ---------------- TASK 3 DECRYPT ----------------
-void decryptText()
+void decryptBitwise()
 {
-    ifstream fin("data.bin", ios::binary);
-    if (!fin)
-    {
-        cout << "File not found\n";
-        return;
-    }
+    ifstream f("bitwise.bin", ios::binary);
+    if (!f) { cout << "File not found\n"; return; }
 
-    vector<string> text(16, "    ");
+    vector<string> s(ROWS, "    ");
 
-    for (int i = 0; i < 64; i++)
+    for (int k = 0; k < ROWS * COLS; k++)
     {
         unsigned short w;
-        fin.read((char*)&w, sizeof(w));
+        f.read((char*)&w, sizeof(w));
 
         int row = w & 15;
-        int hi = (w >> 4) & 15;   //  15 1111
+        int hi = (w >> 4) & 15;
         int p1 = (w >> 8) & 1;
-        int lo = (w >> 9) & 15 ;   //  1010 1111 0100 0101 >>9 0000 0000 0101 0111
+        int lo = (w >> 9) & 15;
         int pos = (w >> 13) & 3;
         int p2 = (w >> 15) & 1;
 
-        int c1 = parity(w & ~(1 << 8), 0, 7);   //        0000 0000 0000 0001 <<8  --> 0000 0001 0000 0000  ~ 1111 1110 1111 1111
-        int c2 = parity(w & ~(1 << 15), 9, 14);    //     1010 1111 0100 0101  ---> 1010 1110 
+        int c1 = parity(w & ~(1 << 8), 0, 7);
+        int c2 = parity(w & ~(1 << 15), 9, 14);
 
         if (p1 != c1 || p2 != c2)
-            cout << "Parity error in word " << i + 1 << endl;
+            cout << "Parity error in word " << k + 1 << endl;
 
-        char ch = (hi << 4) | lo;
-        text[row][pos] = ch;
+        s[row][pos] = char((hi << 4) | lo);
     }
 
-    fin.close();
+    f.close();
 
-    ofstream fout("decoded.txt");
-    for (int i = 0; i < 16; i++)
+    ofstream out("decoded_bitwise.txt");
+    for (int i = 0; i < ROWS; i++)
     {
-        cout << text[i] << endl;
-        fout << text[i] << endl;
+        cout << s[i] << endl;
+        out << s[i] << endl;
     }
-    fout.close();
+    out.close();
 }
 
-// ---------------- TASK 4 CRC ----------------
-void checkCRC()
+// ---------------- TASK 3 ----------------
+struct Bits
 {
-    int n;
-    cout << "How many words? ";
-    cin >> n;
+    unsigned row : 4;
+    unsigned hi : 4;
+    unsigned p1 : 1;
+    unsigned lo : 4;
+    unsigned pos : 2;
+    unsigned p2 : 1;
+};
 
-    for (int k = 0; k < n; k++)
-    {
-        unsigned short w;
-        cout << "Enter word " << k + 1 << ": ";
-        cin >> w;
+union Word
+{
+    Bits b;
+    unsigned short value;
+};
 
-        int odd = 0, even = 0;
-        for (int i = 0; i < 14; i++)
+void encryptStruct()
+{
+    vector<string> s = inputText();
+    ofstream f("struct.bin", ios::binary);
+
+    for (int i = 0; i < ROWS; i++)
+        for (int j = 0; j < COLS; j++)
         {
-            if (i % 2 == 0) even ^= (w >> i) & 1;
-            else odd ^= (w >> i) & 1;
+            Word w;
+            w.value = 0;
+            w.b.row = i;
+            w.b.hi = (s[i][j] >> 4) & 15;
+            w.b.p1 = parity(w.value, 0, 7);
+            w.b.lo = s[i][j] & 15;
+            w.b.pos = j;
+            w.b.p2 = parity(w.value, 9, 14);
+            f.write((char*)&w.value, sizeof(w.value));
         }
 
-        int b14 = (w >> 14) & 1;
-        int b15 = (w >> 15) & 1;
-
-        if (b14 == odd && b15 == even)
-            cout << "Word " << k + 1 << ": OK\n";
-        else
-            cout << "Word " << k + 1 << ": ERROR\n";
-    }
+    f.close();
+    cout << "Saved to struct.bin\n";
 }
 
-// ---------------- MAIN ----------------
+void decryptStruct()
+{
+    ifstream f("struct.bin", ios::binary);
+    if (!f) { cout << "File not found\n"; return; }
+
+    vector<string> s(ROWS, "    ");
+
+    for (int k = 0; k < ROWS * COLS; k++)
+    {
+        Word w;
+        f.read((char*)&w.value, sizeof(w.value));
+
+        int c1 = parity(w.value & ~(1 << 8), 0, 7);
+        int c2 = parity(w.value & ~(1 << 15), 9, 14);
+
+        if (w.b.p1 != c1 || w.b.p2 != c2)
+            cout << "Parity error in word " << k + 1 << endl;
+
+        s[w.b.row][w.b.pos] = char((w.b.hi << 4) | w.b.lo);
+    }
+
+    f.close();
+
+    ofstream out("decoded_struct.txt");
+    for (int i = 0; i < ROWS; i++)
+    {
+        cout << s[i] << endl;
+        out << s[i] << endl;
+    }
+    out.close();
+}
+
+// ---------------- MENU ----------------
 int main()
 {
     int ch;
     do
     {
         cout << "\n1 - Expression\n";
-        cout << "2 - Encrypt text\n";
-        cout << "3 - Decrypt text\n";
-        cout << "4 - Check CRC\n";
+        cout << "2 - Encrypt bitwise\n";
+        cout << "3 - Decrypt bitwise\n";
+        cout << "4 - Encrypt struct/union\n";
+        cout << "5 - Decrypt struct/union\n";
         cout << "0 - Exit\n";
         cout << "Choice: ";
         cin >> ch;
 
         if (ch == 1) task1();
-        else if (ch == 2) encryptText();
-        else if (ch == 3) decryptText();
-        else if (ch == 4) checkCRC();
+        else if (ch == 2) encryptBitwise();
+        else if (ch == 3) decryptBitwise();
+        else if (ch == 4) encryptStruct();
+        else if (ch == 5) decryptStruct();
 
     } while (ch != 0);
 
     return 0;
 }
-
-    } while (choice != 0);
-
-    return 0;
-}
-
